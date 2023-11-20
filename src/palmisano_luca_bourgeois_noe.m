d5bf7f9bd494148f96813df6c526e889fbc92809
Lucino772 <lucapalmi772@gmail.com>
2023-11-20 11:42:35 +0100

    feat: added figures for question 3

--- src/palmisano_luca_bourgeois_noe.m
+++ src/palmisano_luca_bourgeois_noe.m
@@ -9,7 +9,6 @@ alpha = 0.3; % Albedo of the planet
 sigma = 5.67e-8; % Stefan-Boltzmann constant (W/m^2/K^4)
 T0 = 273.15; % Starting from freezing point of water
 tspan = [0, 100]; % Simulate for 100 years
-epsilon = 0.61; % Emissivity factor
 
 % Solve and plot for basic EBM
 [t, T] = ode45(@(t, T) (Q * (1 - alpha) - sigma * T^4) / R, tspan, T0);
@@ -32,8 +31,21 @@ ylabel('Temperature (K)');
 legend('Alpha = 0', 'Alpha = 1');
 
 % EBM with OLR
+epsilon = 0.00; % Emissivity factor
 [t3, T3] = ode45(@(t, T) (Q * (1 - alpha) - epsilon * sigma * T^4) / R, tspan, T0);
+figure; % Open a new figure window
 plot(t3, T3);
+title('EBM with OLR (ε = 0)');
+xlabel('Time (years)');
+ylabel('Temperature (K)');
+
+epsilons = [0.61, 0.76, 0.95]; % Emissivity factor
+[t4, T4] = ode45(@(t, T) (Q * (1 - alpha) - epsilons(1) * sigma * T^4) / R, tspan, T0);
+[t5, T5] = ode45(@(t, T) (Q * (1 - alpha) - epsilons(2) * sigma * T^4) / R, tspan, T0);
+[t6, T6] = ode45(@(t, T) (Q * (1 - alpha) - epsilons(3) * sigma * T^4) / R, tspan, T0);
+figure; % Open a new figure window
+plot(t4, T4, 'r', t5, T5, 'b', t6, T6, 'g');
 title('EBM with OLR');
+legend('ε = 0.61', 'ε = 0.76', 'ε = 0.95');
 xlabel('Time (years)');
 ylabel('Temperature (K)');
