185af5bfa82c48c22dc386ccc8e98c3bb3f8be68
Lucino772 <lucapalmi772@gmail.com>
2023-11-22 16:44:18 +0100

    refactor: updated section for question 1

--- src/palmisano_luca_bourgeois_noe.m
+++ src/palmisano_luca_bourgeois_noe.m
@@ -1,22 +1,22 @@
 % Global Temperature Modeling (INFO-F305)
 % This script contains models for simulating the global average temperature.
 
+%%% CONSTANTS %%%
+R = 2.912;                          % Heat capacity (W-yr/m^2/K)
+Q = 342;                            % Average global solar radiation (W/m^2)
+alpha = 0.3;                        % Albedo of the planet
+sigma = 5.67e-8;                    % Stefan-Boltzmann constant (W/m^2/K^4)
+T0 = 0;                             % Starting from freezing point of water
+tspan = [0, 100];                   % Simulate for 100 years
 
-% Define constants and initial conditions
-R = 2.912; % Heat capacity (W-yr/m^2/K)
-Q = 342;   % Average global solar radiation (W/m^2)
-alpha = 0.3; % Albedo of the planet
-sigma = 5.67e-8; % Stefan-Boltzmann constant (W/m^2/K^4)
-T0 = 273.15; % Starting from freezing point of water
-tspan = [0, 100]; % Simulate for 100 years
-
-% Solve and plot for basic EBM
+%%% QUESTION 1 %%%
 [t, T] = ode45(@(t, T) (Q * (1 - alpha) - sigma * T^4) / R, tspan, T0);
 plot(t, T);
 title('Basic EBM');
 xlabel('Time (years)');
 ylabel('Temperature (K)');
 
+%%% QUESTION 2 %%%
 % Solve and plot for varying albedo
 tspan = [0, 100]; % Simulate for 1000 years
 alpha1 = 0;
